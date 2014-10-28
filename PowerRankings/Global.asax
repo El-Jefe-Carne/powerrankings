c28ae33f5cf9a097a6206c3bc8eb6c4bbaacd819
El_Jefe_Carne <jeff.budnick@gmail.com>
2014-10-27 20:56:14 -0400

    Intial push

---
+++ PowerRankings/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PowerRankings.MvcApplication" Language="C#" %>
---
+++ PowerRankings/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PowerRankings.Startup))]
+namespace PowerRankings
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
